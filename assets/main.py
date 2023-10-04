cfe089487cc3668c0467ad2068d403279792f53e
PaulJkr <paulomondi027@gmail.com>
2023-10-04 14:03:48 +0300

    drawings

--- assets/main.py
+++ assets/main.py
@@ -3,7 +3,7 @@ import	os
 import time
 import random
 
-WIDTH , HEIGHT = 800, 800
+WIDTH , HEIGHT = 700, 700
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter 3D")
 
@@ -18,4 +18,26 @@ BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH,HEIGHT))
+
+def main():
+    run = True
+    FPS = 70
+    clock = pygame.time.Clock()
+
+    def redraw_window():
+        WIN.blit(BG, (0,0))
+        pygame.display.update()
+
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+
+        for event in pygame.event.get():
+            if event.type ==pygame.QUIT:
+                run = False
+
+main()
+
+
+
