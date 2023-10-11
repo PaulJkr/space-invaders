14ed2790d1f8114b81ab2ee6c93fd0d29a63efaa
PaulJkr <paulomondi027@gmail.com>
2023-10-11 13:14:25 +0300

    moving enemies

--- main.py
+++ main.py
@@ -48,7 +48,9 @@ class Player (Ship):
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
 
-
+class Enemy(Ship):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
 
 def main():
     run = True
